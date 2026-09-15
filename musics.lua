4c5ac76e662fb6cced29acc985b78080e24cb99f
Luckzz <324311496+luckzxz@users.noreply.github.com>
2026-09-15 00:06:37 -0300

    Add music: joga joga

--- musics.lua
+++ musics.lua
@@ -480,7 +480,8 @@ return {
         {name = "MONTAGEM TUIM IRRITANTE 3 - MC Geday & MC Miran ( DJ", id = "74256017407052"},
         {name = "repete novihA", id = "106536137601932"},
         {name = "me apaixonei", id = "132901244600372"},
-        {name = "México funk", id = "107553113067574"}
+        {name = "México funk", id = "107553113067574"},
+        {name = "joga joga", id = "109330943148950"}
     },
     ["Phonk"] = {
         {name = "wyles", id = "85385155970460"},
