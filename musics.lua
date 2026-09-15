06fc6bcc1d0b129ec22371e75b711850d66eb5dc
Luckzz <324311496+luckzxz@users.noreply.github.com>
2026-09-15 00:02:34 -0300

    Add music: repete novihA

--- musics.lua
+++ musics.lua
@@ -477,7 +477,8 @@ return {
         {name = "MCHAHA VEM DE KUNIMIN (CLXYAL X CZZ)", id = "102838230236387"},
         {name = "BROTA NA DZ7 - MC 4W MC ROGE ( DJ GH7 ) MANDELÃO CLASSE A - ( BIELXP7 )", id = "91718655037908"},
         {name = "ONDA DO BERIMBAU - STATUS TIK TOK - MC Menor MT, DJ Eduardo [Vtzin]", id = "90728144562197"},
-        {name = "MONTAGEM TUIM IRRITANTE 3 - MC Geday & MC Miran ( DJ", id = "74256017407052"}
+        {name = "MONTAGEM TUIM IRRITANTE 3 - MC Geday & MC Miran ( DJ", id = "74256017407052"},
+        {name = "repete novihA", id = "106536137601932"}
     },
     ["Phonk"] = {
         {name = "wyles", id = "85385155970460"},
