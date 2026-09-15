1292f3f7a7110f09ac3c842f8e8c6d062ec5cfc9
Luckzz <324311496+luckzxz@users.noreply.github.com>
2026-09-14 23:38:33 -0300

    Add music: 1

--- musics.lua
+++ musics.lua
@@ -22,7 +22,8 @@ return {
         {name = "PARA NÃO ESQUECER QUEM SOMOS", id = "88937498361674"},
         {name = "Uno zero", id = "112959083808887"},
         {name = "Iate do neymar", id = "135738534706063"},
-        {name = "Batidao na aldeia", id = "79953696595578"}
+        {name = "Batidao na aldeia", id = "79953696595578"},
+        {name = "1", id = "1"}
     },
     ["Trap"] = {
         {name = "Fumadouro Freestyle (Igorivooz)", id = "138361625716987"},
