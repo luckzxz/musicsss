2e3744677fb62f20cac0cdfecffda14ec06e851a
Luckzz <324311496+luckzxz@users.noreply.github.com>
2026-09-20 11:18:57 -0300

    Add music: subiu

--- musics.lua
+++ musics.lua
@@ -66,6 +66,7 @@ return {
         {name = "descaso", id = "95674923139820"}
     },
     ["Funk"] = {
+        {name = "subiu", id = "71793041030601"},
         {name = "eu só quero", id = "122369808511994"},
         {name = "nova", id = "107174883653819"},
         {name = "foi só socada", id = "124950663117359"},
