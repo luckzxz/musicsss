18f94968aa5d7f3a188404b63b186e9416b58500
Luckzz <324311496+luckzxz@users.noreply.github.com>
2026-09-20 16:17:16 -0300

    Add music: hope xxxtentacion

--- musics.lua
+++ musics.lua
@@ -67,6 +67,7 @@ return {
         {name = "descaso", id = "95674923139820"}
     },
     ["Funk"] = {
+        {name = "hope xxxtentacion", id = "99551853291230"},
         {name = "subiu", id = "71793041030601"},
         {name = "eu só quero", id = "122369808511994"},
         {name = "nova", id = "107174883653819"},
